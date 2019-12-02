3144b6a5667e814e33c3e94782683ad1c9c9eeed
Pablo <pilj@virket.com>
2019-12-02 11:08:01 -0600

    Main Layout

---
+++ resources/views/layouts/__includes/main-footer.blade.php
@@ -0,0 +1,13 @@
+<footer class="footer">
+    <div class="container-fluid">
+        <nav class="float-left">
+            <ul>
+                <li>
+                    <a href="https://virket.com/">
+                        VIRKET
+                    </a>
+                </li>
+            </ul>
+        </nav>
+    </div>
+</footer>
---
+++ resources/views/layouts/__includes/menu.blade.php
@@ -0,0 +1,29 @@
+<div class="sidebar" data-color="azure" data-background-color="white">
+    <div class="logo">
+        <a href="#" class="simple-text logo-normal">
+            Deals NetSuite
+        </a>
+    </div>
+    <div class="sidebar-wrapper">
+        <ul class="nav">
+            <li class="nav-item  ">
+                <a class="nav-link" href="./dashboard.html">
+                    <i class="material-icons">dashboard</i>
+                    <p>Tickets</p>
+                </a>
+            </li>
+            <li class="nav-item ">
+                <a class="nav-link" href="./user.html">
+                    <i class="material-icons">person</i>
+                    <p>Tareas</p>
+                </a>
+            </li>
+            <li class="nav-item active ">
+                <a class="nav-link" href="./tables.html">
+                    <i class="material-icons">content_paste</i>
+                    <p>Sales Ordes</p>
+                </a>
+            </li>
+        </ul>
+    </div>
+</div>
