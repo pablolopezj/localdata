ebc87cbd4f5cfdf3048954387baf960e2b6fc125
Pablo <pilj@virket.com>
2020-01-08 15:07:45 -0600

    Procesing false

--- resources/views/tasks/tasks-godaddy.blade.php
+++ resources/views/tasks/tasks-godaddy.blade.php
@@ -67,32 +67,6 @@
         </div>
     </div>
 </div>
-
-<!-- /.modal -->
-
-<div class="modal fade" id="modal-xl">
-    <div class="modal-dialog modal-xl">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h4 class="modal-title">Tarea</h4>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-            </div>
-            <div class="modal-body">
-                <p class="main-content">
-
-                </p>
-            </div>
-            <div class="modal-footer justify-content-between">
-                <button type="button" class="btn btn-primary" data-dismiss="modal">Close</button>
-            </div>
-        </div>
-        <!-- /.modal-content -->
-    </div>
-    <!-- /.modal-dialog -->
-</div>
-<!-- /.modal -->
 @endsection
 
 @section('scripts')
@@ -103,7 +77,7 @@
 
 
         $('#tasks').DataTable({
-            processing: true,
+            processing: false,
             serverSide: true,
             ajax: '/godaddy/feed',
             "columns": [{
